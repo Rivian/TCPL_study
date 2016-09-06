d567aac9812e7d1796fd15fc0bdbd2b482bcd400
Rivian <rivian@gmail.com>
2016-09-06 19:07:19 +0900

    dcl.c and dcl.h modified as practice 5.19

--- dcl.h
+++ dcl.h
@@ -8,9 +8,11 @@
 char *dtype[] = { "char", "int", "float", "double", "long" };
 
 enum { NAME, PARENS, BRACKETS };
+enum { NO, YES };
 
 void dcl( void );
 void dirdcl( void );
+void errmsg( char * );
 
 int gettoken( void );
 int tokentype;			/* type of last token */
@@ -18,6 +20,7 @@ char token[MAXTOKEN];		/* last token string */
 char name[MAXTOKEN];		/* identifier name */
 char datatype[MAXTOKEN];	/* data type = char, int, etc. */
 char out[1000];			/* output string */
+int prevtoken = NO;		/* there is no previous token */
 
 int isexist( char **, char * );
 
@@ -26,6 +29,12 @@ int gettoken( void )
         int c, getch( void );
         void ungetch( int );
         char *p = token;
+
+	if( prevtoken == YES )
+	{
+		prevtoken = NO;
+		return tokentype;
+	}
 
         while( (c = getch()) == ' ' || c == '\t' )
                 ;
@@ -74,4 +83,50 @@ int isexist( char *dtype[], char *token )
 			return 1;
 
 	return 0;
+}
+
+/* dcl : parse a declarator */
+void dcl( void )
+{
+        int ns;
+
+        for( ns = 0; gettoken() == '*'; ) /* count *'s */
+                ns++;
+
+        dirdcl();
+        while( ns-- > 0 )
+                strcat( out, " pointer to" );
+}
+
+/* dirdcl : parse a direct declarator */
+void dirdcl( void )
+{
+        int type;
+
+        if( tokentype == '(' )  /* dcl */
+        {
+                dcl();
+                if( tokentype != ')' )
+                        errmsg("error : missing )\n");
+        }
+        else if( tokentype == NAME )    /* variable name */
+                strcpy( name, token );
+        else
+                errmsg("error : expected name or (dcl)\n");
+
+        while( (type=gettoken()) == PARENS || type == BRACKETS )
+                if( type == PARENS )
+                        strcat( out, " function returning " );
+                else
+                {
+                        strcat( out, " array");
+                        strcat( out, token );
+                        strcat( out, " of");
+                }
+}
+
+void errmsg( char *msg )
+{
+        printf("%s", msg );
+        prevtoken = YES;
 }
